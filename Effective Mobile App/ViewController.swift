ac2337831cd9f2147d98e785814f2ec7f48c8c87
Nikita <kaska0093@inbox.ru>
2024-08-31 06:52:50 -0700

    Initial Commit

---
+++ Effective Mobile App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Effective Mobile App
+//
+//  Created by Nikita Shestakov on 31.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
